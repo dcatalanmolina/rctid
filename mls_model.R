ab310b336aab72fb4bf0d7b70bd5f741e8a6c560
dcatalanmolina <132309569+dcatalanmolina@users.noreply.github.com>
2024-08-06 15:05:46 -0700

    First model draft

---
+++ mls_model.R
@@ -0,0 +1,43 @@
+##############################
+##### mls match prediction ###
+###########################
+
+# 1. Libraries and Data ----------------------
+library(tidyverse)
+library(brms)
+
+matches <- 
+  tibble(
+    team = paste("team ", 1:10, sep = ""),
+    home = rep(c("home", "away"), times = 5),
+    game = rep(1:5, each = 2),
+    goals = 
+      sample(
+        c(0,1,2,3), 
+        size = 10,
+        replace =  T,
+        prob = c(.20,.50, .20, .10)
+        )
+  )
+
+# 2. EMpty model ---------
+m0 <- 
+  brm(
+    goals ~ 1 + (1|team),
+    data = matches,
+    warmup = 100,
+    iter = 1000,
+    seed = 3
+  )
+
+# 3. home field advantage ------
+m1 <- 
+  brm(
+    goals ~ 1 + home + (1|team),
+    data = matches,
+    warmup = 100,
+    iter = 1000,
+    seed = 3
+  )
+
+
