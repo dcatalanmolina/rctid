13c9d8a8f1359d525c29de01404b780228c494c9
dcm <minga.data@gmail.com>
2024-08-15 17:43:58 -0700

    Cleans script and adds predictions for the remainder of the season

--- predict_matches.R
+++ predict_matches.R
@@ -2,7 +2,7 @@
 ## Predict MLS Matches ######
 ############################
 
-# 1. Libraries and model outputs ---------
+# 1. Libraries, data, and model outputs ---------
 library(tidyverse)
 library(brms)
 library(tidybayes)
@@ -10,15 +10,38 @@ library(tidybayes)
 m2 <- readRDS("models/m2.rds")
 m2.1 <- readRDS("models/m2_1.rds")
 
+# funs
+source("posterior_matchup_summ.R")
+source("expected_standings.R")
+
 # get match df
-mls_wide <- readRDS("data/mls_wide.rds")
+mls_wide <- 
+  readRDS("data/mls_wide.rds") %>% 
+  mutate(
+    a_home_points = #actual
+      case_when(
+        goal_diff == 0 ~ 1, #draw
+        goal_diff > 0 ~ 3, #win
+        goal_diff < 0 ~ 0, #lose
+        T ~ NA_integer_ #future match
+      ),
+    a_away_points = 
+      case_when(
+        goal_diff == 0 ~ 1, #draw
+        goal_diff < 0 ~ 3, #win
+        goal_diff > 0 ~ 0, #lose
+        T ~ NA_integer_ #future match
+      )
+  )
+
 
 # get future matches
 future_matches <- 
   mls_wide %>% 
-  filter(Date > today())
+  filter(Date > today()) %>% 
+  mutate(matchup = paste(Home, "vs", Away, sep = " "))
 
-# 2. Predict single match ---------------
+# 2. Predict single match ------------------------------------
 next_match <- 
   tibble(
     Home = "St. Louis",
@@ -44,10 +67,9 @@ pred_match %>%
 # 3. Predict next week --------------
 week_28 <- 
   future_matches %>% 
-  filter(Wk == "28") %>% 
-  mutate(matchup = paste(Home, "vs", Away, sep = " "))
+  filter(Wk == "28")
 
-# two ways of estimating pred values
+# two sets of expected values, based on different models
 week_28_preds <- 
   add_epred_draws(
     week_28,
@@ -60,49 +82,52 @@ week_28_preds_2.1 <-
     m2.1
   )
 
-posterior_matchup_preds <- 
-  function(df) {
-    
-    pred_distribution <- 
-      quantile(df$.epred, probs = c(.10, .25, .50, .75, .90))
-    
-    matchup_probs <- 
-      df %>% 
-      summarise(
-        pr_home_win = mean(if_else(.epred > 0, 1, 0)),
-        pr_upset = mean(if_else(.epred < 0, 1, 0))
-      )
-    
-    pred_summary <- 
-      list(
-        pred_distribution = pred_distribution,
-        matchup_probs = matchup_probs
-      )
-    
-    return(pred_summary)
+# summarize predictions
+week_28_preds_summary_m2 <- 
+  posterior_matchup_summ(week_28_preds)
+
+week_28_preds_summary_m2.1 <- 
+  posterior_matchup_summ(week_28_preds_2.1)
   
-}
+saveRDS(week_28_preds_summary_m2, "models/m2_wk28_preds.rds")
+saveRDS(week_28_preds_summary_m2.1, "models/m21_wk28_preds.rds")
 
-week_28_preds_summary <- 
-  map(
-    .x = unique(week_28$matchup),
-    ~week_28_preds %>% 
-      filter(matchup == .x) %>% 
-      posterior_matchup_preds(.)
+# 4. Predict all remaining matches --------------
+
+# two sets of expected values, based on different models
+future_preds_m2 <- 
+  add_epred_draws(
+    future_matches,
+    m2
   )
 
-names(week_28_preds_summary) <- unique(week_28$matchup)
-
-saveRDS(week_28_preds_summary, "models/m2_wk28_preds.rds")
-
-week_28_preds_summary_2.1 <- 
-  map(
-    .x = unique(week_28$matchup),
-    ~week_28_preds_2.1 %>% 
-      filter(matchup == .x) %>% 
-      posterior_matchup_preds(.)
+future_preds_m2.1 <- 
+  add_epred_draws(
+    future_matches,
+    m2.1
   )
 
-names(week_28_preds_summary_2.1) <- unique(week_28$matchup)
+saveRDS(future_preds_m2, paste("models/m2_future_preds_", today(), ".rds", sep = ""))
+saveRDS(future_preds_m2.1, paste("models/m21_future_preds_", today(), ".rds", sep = ""))
 
-saveRDS(week_28_preds_summary_2.1, "models/m2_wk28_preds_2_1.rds")
+# posterior summaries
+future_preds_m2_summary <- 
+  posterior_matchup_summ(future_preds_m2)
+ 
+future_preds_m21_summary <- 
+  posterior_matchup_summ(future_preds_m2.1)
+
+standings_m2 <- 
+  expected_standings(
+    mls_wide, future_matches, 
+    future_preds_m2_summary
+  )
+
+standings_m21 <- 
+  expected_standings(
+    mls_wide, future_matches, 
+    future_preds_m21_summary
+  )
+
+saveRDS(standings_m2, "models/standings_m2.rds")
+saveRDS(standings_m21, "models/standings_m21.rds")
