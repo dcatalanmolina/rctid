2b4af690fa4f288d7f29a6cd76fb1d7a7a6192ac
dcatalanmolina <dcatalanmolina@gmail.com>
2025-05-20 16:02:14 -0700

    Splitting steps into individual modules.

---
+++ get_2025_football_data.R
@@ -0,0 +1,29 @@
+######################################
+# Getting 2025 Football Data
+#####################################
+
+library(httr2)
+library(tidyverse)
+library(worldfootballR)
+
+# Match Results from FBref ------------------------
+
+## MLS -----
+mls_25 <- 
+  fb_match_results(
+    country = "USA", gender = "M", season_end_year = 2025, tier = "1st"
+  )
+
+saveRDS(mls_25, "data/mls_25.rds")
+
+# Season-level stats from FBref -------------------
+
+## MLS
+mls_25_defense <- 
+  fb_season_team_stats(
+    country = "USA", gender = "M", 
+    season_end_year = 2025, tier = "1st",
+    stat_type = "defense"
+  )
+
+saveRDS(mls_25_defense, "data/mls_25_defense.rds")
