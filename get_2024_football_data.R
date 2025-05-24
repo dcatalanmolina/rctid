bdc4e57e3a77e983f037ca996c9b46ab1cad925b
compa-dcm <168883875+compa-dcm@users.noreply.github.com>
2025-05-24 07:35:21 -0700

    Update get_2024_football_data.R
    Gets shooting and standard stats.

--- get_2024_football_data.R
+++ get_2024_football_data.R
@@ -26,4 +26,20 @@ mls_24_defense <-
     stat_type = "defense"
   )
 
+mls_24_shooting <- 
+  fb_season_team_stats(
+    country = "USA", gender = "M", 
+    season_end_year = 2024, tier = "1st",
+    stat_type = "shooting"
+  )
+
+mls_24_standard <- 
+  fb_season_team_stats(
+    country = "USA", gender = "M", 
+    season_end_year = 2024, tier = "1st",
+    stat_type = "standard"
+  )
+
 saveRDS(mls_24_defense, "data/mls_24_defense.rds")
+saveRDS(mls_24_shooting, "data/mls_24_shooting.rds")
+saveRDS(mls_24_standard, "data/mls_24_standard.rds")
